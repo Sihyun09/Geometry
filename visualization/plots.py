d30023e904ac7cfc8325e74ae546fdfec2e7b50d
Sihyun <sihyun6960@gmail.com>
2026-06-16 12:16:53 +0900

    line

--- visualization/plots.py
+++ visualization/plots.py
@@ -20,19 +20,30 @@ def vessel_plot(
 
     if Fx is not None:
 
-        for i in range(
-            0,
-            len(x),
-            8
-        ):
+        scale = 0.7
+
+        for i in range(0, len(x), 8):
+
+            x0 = x[i]
+            y0 = y[i]
+
+            x1 = x0 + scale * Fx[i]
+            y1 = y0 + scale * Fy[i]
+
+            fig.add_trace(
+                go.Scatter(
+                    x=[x0, x1],
+                    y=[y0, y1],
+                    mode="lines",
+                    showlegend=False
+                )
+            )
 
             fig.add_annotation(
-                x=x[i] + 0.4 * Fx[i],
-                y=y[i] + 0.4 * Fy[i],
-
-                ax=x[i],
-                ay=y[i],
-
+                x=x1,
+                y=y1,
+                ax=x0,
+                ay=y0,
                 showarrow=True,
                 arrowhead=3
             )
